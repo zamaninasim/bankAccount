d7d8f240621860079fd0f0d7c51e868be02f1366
nasim <zamaninasim213@gamail.com>
2021-11-24 19:14:28 +0330

    add project classes

--- src/ir/maktab/model/User.java
+++ src/ir/maktab/model/User.java
@@ -1,6 +1,75 @@
 package ir.maktab.model;
 
-public class User {
+import ir.maktab.accounts.Account;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String name;
+    private String lastname;
+    private String nationalCode;
+    private String address;
+    private String postalCode;
+    private String phoneNumber;
+    private String fatherName;
+    private List<Account> accounts = new ArrayList<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
 
 }
