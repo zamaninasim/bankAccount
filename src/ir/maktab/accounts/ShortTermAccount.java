50a7fc038582d59f353fa5852a785d1850d5ed5c
nasim <zamaninasim213@gamail.com>
2021-11-24 19:20:20 +0330

    add shortterm account property

--- src/ir/maktab/accounts/ShortTermAccount.java
+++ src/ir/maktab/accounts/ShortTermAccount.java
@@ -1,5 +1,36 @@
 package ir.maktab.accounts;
 
-public class ShortTermAccount extends Account {
+import ir.maktab.model.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
+public class ShortTermAccount extends Account {
+    private static final double monthlyProfit = 0.1;
+
+    public ShortTermAccount() {
+    }
+
+    public ShortTermAccount(long amount, User user) {
+        super(amount, user);
+        calculateInterests();
+    }
+
+    public void calculateInterests() {
+        LocalDate currentDate = calculateDateOfToday();
+        long daysBetween = ChronoUnit.DAYS.between(super.getAccountOpeningDate(),currentDate);
+        long mounts = daysBetween / 30;
+        double profit =mounts*(super.getBalance()*monthlyProfit);
+        super.setBalance(super.getBalance() + (long) profit);
+    }
+
+    @Override
+    public String toString() {
+        return "***ShortTermAccount***" +
+                "\nwelcome " + super.getUser().getName() + " " + super.getUser().getLastname()
+                + "\nyour accountId is=" + super.getAccountId() +
+                "\nyour balance is=" + super.getBalance() +
+                "\nyour account open in=" + super.getAccountOpeningDate();
+    }
 }
