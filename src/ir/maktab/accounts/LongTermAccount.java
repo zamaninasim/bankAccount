53695a511d320f63639be16cae3d6a8f5c6563b1
nasim <zamaninasim213@gamail.com>
2021-11-24 19:19:14 +0330

    add longterm account property

--- src/ir/maktab/accounts/LongTermAccount.java
+++ src/ir/maktab/accounts/LongTermAccount.java
@@ -1,5 +1,18 @@
 package ir.maktab.accounts;
 
-public class LongTermAccount extends Account {
+import ir.maktab.model.User;
 
+public class LongTermAccount extends Account {
+    public LongTermAccount(long amount, User user) {
+        super(amount, user);
+    }
+
+    @Override
+    public String toString() {
+        return "***LongTermAccount***" +
+                "\nwelcome " + super.getUser().getName() + " " + super.getUser().getLastname()
+                + "\nyour accountId is=" + super.getAccountId() +
+                "\nyour balance is=" + super.getBalance() +
+                "\nyour account open in=" + super.getAccountOpeningDate();
+    }
 }
